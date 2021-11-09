b435149bb4775e036e9b6e4c639ad4072fdc458f
Ferhuzje <86949390+Ferhuzje@users.noreply.github.com>
2021-11-09 12:18:40 +0100

    Konto und Program

---
+++ ThreadSperre/src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) throws Exception {
+        // 1. Konto mit einem Guthaben = 100
+        // 2. Geldkarten/EC-Karte heben gleichzeitig Geld ab
+
+        Konto konto = new Konto(100)
+
+    }
+}
